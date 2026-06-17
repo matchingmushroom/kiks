0ee8417956934bc5e6bd05a70233f23428b56c90
mukun <mukun@example.com>
2026-06-17 18:44:56 +0545

    deploy: update site

---
+++ _next/static/He3ta60ZfOWTHsB57svK4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fadmin\u002Finvoices\u002F[id]","\u002Fadmin\u002Fsales\u002F[id]","\u002Finvoice\u002F[id]","\u002Fproduct\u002F[id]","\u002Fproducts\u002F[category]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
